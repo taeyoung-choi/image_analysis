d2b542df0515cd558d24908596adcf1f74e13fdf
TaeYoung Choi <tc2777@columbia.edu>
2018-02-19 23:22:40 -0500

    finished assignment 1: comments required

---
+++ project 1/README.md
@@ -0,0 +1,19 @@
+## Assignment 1: practice an image segmentation algorithm using K-means
+
+##### TaeYoung Choi (tc2777)
+##### Monday February 19, 2018
+
+1. The parameter K was chosen from the histogram. The assumption is that the centroids will converge to the pixel 
+values that appear often. If the number of clusters exceeds the number of peaks in the histogram would be the 
+algorithm unstable, because the extra clusters will appear in different locations each iteration. Thus it was 
+reasonable to pick K according to the shape of histogram.
+
+2. After running K-means, it was necessary to check which color corresponds to the face. This was something manually 
+coded and this can be automated using a smarter algorithm.
+
+3. Face Detection: The region has to be big enough to fit the entire face and small enough to exclude background. The
+ metric I used 
+ 
+<a href="https://www.codecogs.com/eqnedit.php?latex=\frac{\mathrm{number\&space;of\&space;blue\&space;pixels}}{\mathrm{total\&space;number\&space;of\&space;blue\&space;pixels}}" target="_blank">
+<img src="https://latex.codecogs.com/gif.latex?\frac{\mathrm{number\&space;of\&space;blue\&space;pixels}}{\mathrm{total\&space;number\&space;of\&space;blue\&space;pixels}}" 
+title="\frac{\mathrm{number\ of\ blue\ pixels}}{\mathrm{total\ number\ of\ blue\ pixels}}" /></a>
--- project 1/tc2777_TaeYoung_kmeans.py
+++ project 1/tc2777_TaeYoung_kmeans.py
@@ -4,48 +4,97 @@ import numpy as np
 from scipy.stats import gaussian_kde
 
 
-def to_grey_hist(image):
-    grey = image.convert("L")
-    grey_hist = np.array(grey.histogram())
-
-    return grey_hist
-
-
 def get_peaks(grey_hist):
+    """
+    Calculates the number and location of peaks in a given histogram
+
+    Parameters
+    ----------
+    grey_hist : numpy array of length 256
+
+    each entry contains how many pixels are there for each gray value from 0 to 255
+
+    Returns
+    -------
+    K : int
+        the number of peaks in a given histogram
+    initial : List(Int) of length K
+        K gray values that peaks appear
+    """
+
     xs = np.linspace(0, 255, 256)
+
+    # transform the input that is appropriate for histogram
     hist = []
     for i in range(256):
         hist.extend([int(xs[i])] * grey_hist[i])
-
     hist = np.array(hist)
+
+    # overlay a density curve to detect peaks
     density = gaussian_kde(hist)
     density.set_bandwidth(0.05)
     density._compute_covariance()
-    plt.plot(xs, density(xs))
-    plt.hist(hist, bins=256, density=True)
-    plt.savefig("data/hist.png")
-    a = np.ediff1d(density(xs))
-    asign = np.sign(a) + 1
 
+    # peaks appear when values stop increasing
+    a = np.ediff1d(density(xs))
+
+    # only count peaks not troughs
+    asign = np.sign(a) + 1
     signchange = (np.roll(asign, 1) - asign) == 2
+    # set the initial point as non-pick
+    # this could occur if the last value is greater than the last value
+    # np.roll moves kth element to k+nth position and
+    # k-nth element to first position
     signchange[0] = False
     signchange = np.append(signchange, False)
     K = sum(signchange)
     initial = xs[signchange]
 
+    # Saving histogram
+    plt.plot(xs, density(xs))
+    plt.hist(hist, bins=256, density=True)
+    plt.savefig("data/hist.png")
+
     return K, initial
 
 
 def k_means(K, initial, grey):
+    """
+    Runs K-means algorithm based on pixel values
+
+    Parameters
+    ----------
+    K: number of clusters
+    initial: K gray values that peaks appear
+    grey: greyscale image
+
+    Returns
+    -------
+    updated : List(Double)
+        centroids calculated after algorithm terminates
+    x_indices : dict
+        x-coordinates of pixels for each cluster
+    y_indices : dict
+        y-coordinates of pixels for each cluster
+    """
+
+    # terminates if the updated centroids are the same as the previous iteration
     while True:
+        # calculator stores pixel values of each cluster
         calculator = {}
+        # x_indices stores x-coordinates of pixels for each cluster
         x_indices = {}
+        # y_indices stores y-coordinates of pixels for each cluster
         y_indices = {}
+        # iterating through all pixels
         (width, height) = grey.size
         for w in range(width):
             for h in range(height):
                 each_pixel = grey.getpixel((w, h))
+                # for each pixel calculate the closest centroid
                 closest = np.argmin(abs(initial - each_pixel))
+                # for new cluster create key in dictionary
+                # otherwise append value to existing list of values
                 if closest in calculator:
                     calculator[closest] = np.append(calculator[closest], each_pixel)
                     x_indices[closest] = np.append(x_indices[closest], w)
@@ -56,10 +105,13 @@ def k_means(K, initial, grey):
                     y_indices[closest] = np.array([h])
         updated = []
         for i in range(K):
+            # update centroid to the average value of clusters
             avg = np.mean(calculator[i])
             updated.insert(i, avg)
+        # update centroids
         updated = np.array(updated)
 
+        # algorithm terminates if centroids do not change
         if np.sum(initial - updated) == 0:
             break
         else:
@@ -68,39 +120,86 @@ def k_means(K, initial, grey):
 
 
 def to_k_colors(image, K, x_indices, y_indices):
+    """
+    Change original image into K coloring
+
+    Parameters
+    ----------
+    image : PIL Image
+        original image
+    K : int
+        number of clusters
+    x_indices : dict
+        x-coordinates of pixels for each cluster
+    y_indices : dict
+        y-coordinates of pixels for each cluster
+    """
+    # candidate colors
     col = {0: (244, 66, 66), 1: (66, 244, 66), 2: (66, 66, 244), 3: (244, 66, 200),
            4: (244, 244, 66), 5: (66, 244, 244), 6: (244, 244, 244), 7: (66, 66, 66),
            8: (0, 0, 0), 9: (244, 244, 244)}
 
+    # put RGB values for each cluster
     for j in range(K):
         x = x_indices[j]
         y = y_indices[j]
         length = len(x_indices[j])
         for k in range(length):
             image.putpixel((x[k], y[k]), col[j])
-
+    # save image
     plt.imshow(image)
     plt.savefig("data/k_color.png")
 
 
 def get_face(image, x_indices, y_indices):
+    """
+    Segment face from image
+
+    Parameters
+    ----------
+    image : PIL Image
+        original image
+    x_indices : dict
+        x-coordinates of pixels for each cluster
+    y_indices : dict
+        y-coordinates of pixels for each cluster
+    """
+
+    # the face should be colored blue
+    # number of blue pixels
     blue = len(x_indices[2])
+
+    # get a region in the image that scores the highest point
     best_score = 0
+    # size of the best region, x-coordinate, y-coordinate
     best_conf = (0, 0, 0)
+
+    # iterate through different regions of different sizes
+    # j indicates the size of candidate region
     for j in range(10, min(image.size[0], image.size[1]), 100):
+        # do not beyond the image size
         up_to_x = image.size[0] - j
         up_to_y = image.size[1] - j
+        # iterate though different starting x, y coordinates
         for k in range(0, up_to_x, 5):
             for r in range(0, up_to_y, 10):
                 score = 0
+                # count the number of blue points within candidate region
                 for q in range(len(y_indices[2])):
                     if (k < x_indices[2][q] < k + j) and (r < y_indices[2][q] < r + j):
                         score += 1
+                # multiply two scores
+                # first: (number of points within candidate region) / (total number of blue points)
+                # second: (number of points within candidate region) / (number of pixels in candidate region)
                 current = (score / blue) * (score / (j * j))
+
+                # keep the best score
                 if current > best_score:
                     best_score = current
                     best_conf = (j, k, r)
+    # size of the best region, x-coordinate, y-coordinate
     (a, b, c) = best_conf
+    # draw a square around the face
     for q in range(a):
         image.putpixel((b + q, c), (0, 0, 0))
         image.putpixel((b, c + q), (0, 0, 0))
@@ -114,11 +213,16 @@ def get_face(image, x_indices, y_indices):
 def main():
     image = Image.open('data/face_d2.jpg')
     image2 = Image.open('data/face_d2.jpg')
+    # work with grayscale image
     grey = image.convert("L")
     grey_hist = np.array(grey.histogram())
+    # calculate the number of clusters
     K, initial = get_peaks(grey_hist)
+    # run K-means algorithm
     centroids, x_indices, y_indices = k_means(K, initial, grey)
+    # turn image into K coloring image
     to_k_colors(image2, K, x_indices, y_indices)
+    # draw a square around the face
     get_face(image, x_indices, y_indices)
 
 
