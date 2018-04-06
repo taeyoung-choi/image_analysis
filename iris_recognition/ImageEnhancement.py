f61fad18da2558aadd6875ae0e88b13388137e04
TaeYoung Choi <tc2777@columbia.edu>
2018-04-05 23:42:29 -0400

    image added

--- iris_recognition/ImageEnhancement.py
+++ iris_recognition/ImageEnhancement.py
@@ -1,19 +1,19 @@
 import cv2
-
+import matplotlib.pyplot as plt
 
 def run(img):
     """
-    It divides the normalized image into 16 by 16 grids and equalizes the histogram of each grid.
+    It divides the normalized image into 32 by 32 grids and equalizes the histogram of each grid.
 
     :param img: normalized iris image
     :return: enhance image by histogram equalization
     """
     # do not change the original image
     img2 = img.copy()
-    size = 16
-    for i in range(4):
-        for j in range(32):
-            # Define each 16 by 16 grid iteratively
+    size = 32
+    for i in range(2):
+        for j in range(16):
+            # Define each 32 by 32 grid iteratively
             start_height = i*size
             end_height = start_height+size
             start_wid = j*size
@@ -21,5 +21,7 @@ def run(img):
             grid = img2[start_height:end_height, start_wid:end_wid]
             # Histogram Equalization
             img2[start_height:end_height, start_wid:end_wid] = cv2.equalizeHist(grid)
+    plt.imshow(img2, cmap="gray")
+    plt.savefig("plot/enhanced.png")
 
     return img2
